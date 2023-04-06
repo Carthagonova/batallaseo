bfa4f74185f39c8f102bb4ba369edbfe983b20e0
Carlos Sánchez Donate <43948976+barvock@users.noreply.github.com>
2023-04-06 13:19:25 +0200

    gsc

--- complements/header.php
+++ complements/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta charset="UTF-8" />
+        <meta name="google-site-verification" content="6GZxu412Z5JjyYavdnIOGQyPivYh65UawhPjXbhEd_s" />
 
         </head>
     <body>
--- core.php
+++ core.php
@@ -6,15 +6,17 @@ $request_uri = $_SERVER['REQUEST_URI'];
 include $_SERVER['DOCUMENT_ROOT'].'/complements/header.php';
 
 // Redirigir a la página correspondiente
-if ($request_uri == '/robots')) {
+if ($request_uri == '/robots') {
     echo "Pagina bloqueada por el robots.txt";
 } elseif ($request_uri == '/error4xx') {
-    header("HTTP/1.0 404 Not Found");
-    echo 'Estás en una página de error';
+    header("HTTP/1.0 418 Not Found");
+    echo 'Soy una tetera';}
+    elseif ($request_uri == '/existir') {
+        echo 'Esta página si existe';
 } else {
     // Mostrar una página de error si la URL solicitada no coincide con ninguna página conocida
     header("HTTP/1.0 404 Not Found");
-    include 'errores/404.php';
+    echo '<h1>Error 404</h1>';
 }
 
 include $_SERVER['DOCUMENT_ROOT'].'/complements/footer.php';
